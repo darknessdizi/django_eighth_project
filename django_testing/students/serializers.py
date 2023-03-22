3e5b300c921c6d80f3903b8cdeb49bb095b92a25
Remezov Dmitriy <darknessdizi@gmail.com>
2023-03-22 23:42:20 +0700

    Finished project

--- django_testing/students/serializers.py
+++ django_testing/students/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
-
 from students.models import Course
+from django.conf import settings
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -8,3 +8,14 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ("id", "name", "students")
+
+
+    def validate(self, data):
+        
+        if self.context["request"].method == 'PATCH':
+            if len(data['students']) <= settings.MAX_STUDENTS_PER_COURSE:
+                return data
+            else:
+                raise serializers.ValidationError("Слишком много студентов")
+        elif self.context["request"].method == 'POST':
+            return data
--- django_testing/tests/conftest.py
+++ django_testing/tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
+
 from rest_framework.test import APIClient
 from model_bakery import baker
-
 from students.models import Student, Course
 
 
@@ -16,15 +16,8 @@ def factory_course():
         return ans
     return course
 
-@pytest.fixture
-def factory_student():
-    def student(*args, **kwargs):
-        return  baker.make(Student, *args, **kwargs)
-    return student
-
 # @pytest.fixture
-# def change_max_students(value, settings):
-#     if value > settings.MAX_STUDENTS_PER_COURSE:
-#         assert False
-#     else:
-#         assert True
+# def factory_student():
+#     def student(*args, **kwargs):
+#         return  baker.make(Student, *args, **kwargs)
+#     return student
